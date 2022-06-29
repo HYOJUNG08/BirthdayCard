ccebb6f62126cdae3decc0a10fc3f34218121e63
alicehjjung <104620291+alicehjjung@users.noreply.github.com>
2022-06-30 01:37:00 +0900

    fix file

--- twilio_credentials.py
+++ twilio_credentials.py
@@ -1,4 +1,4 @@
 #cellphone = ""
-twilio_number = "+19806552132"
-twilio_account = "ACde4a4621f1ea8442ac7150ede234f225"
-twilio_token = "552772613dfa7c713f71ff134cecd6a0"
+twilio_number = ""
+twilio_account = ""
+twilio_token = ""
